6566e566cb0bebf6ca35b27fc3b290841023dda3
dmpatel7 <dmpatel7@ncsu.edu>
2026-05-06 16:31:07 -0700

    file-clean-up

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { open: true },
-  optimizeDeps: { entries: ['index.html'] }
+  server: { open: true }
 });
